643f6545fe9661e52926e49f8a263b263aba2560
Andrew <apwicker@gmail.com>
2023-11-06 14:24:59 -0800

    feat: Add createGame and getCollection methods to gameController
    This commit adds new methods to the gameController file for creating a game and fetching the collection.

--- src/backend/controllers/gameController.ts
+++ src/backend/controllers/gameController.ts
@@ -1,2 +1,66 @@
 import mongoose from 'mongoose';
-const { ga };
+import Game from '../../services/models/gameModel.js';
+import { Request, Response } from 'express';
+
+const mongoURI: string = process.env.MONGO_URI || '';
+
+mongoose.connect(mongoURI);
+
+interface GameData {
+  bggId: number;
+  title: string;
+  coverImage: string;
+  thumbnail: string;
+  description: string;
+  minPlayers: number;
+  maxPlayers: number;
+  yearPublished: number;
+}
+
+const gameController = {
+  createGame: async (req: Request, res: Response, next: Function) => {
+    try {
+      const {
+        bggId,
+        title,
+        coverImage,
+        thumbnail,
+        minPlayers,
+        maxPlayers,
+        yearPublished,
+      }: GameData = req.body;
+
+      const newGame = new Game({
+        bggId,
+        title,
+        coverImage,
+        thumbnail,
+        minPlayers,
+        maxPlayers,
+        yearPublished,
+      });
+
+      await newGame.save();
+      res.status(201).json({ message: 'Game added to collection' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        error: 'An error occurred while adding the game to the collection',
+      });
+    }
+  },
+
+  getCollection: async (req: Request, res: Response, next: Function) => {
+    try {
+      const games = await Game.find({});
+      return res.status(200).json(games);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({
+        error: 'An error occurred while fetching the collection',
+      });
+    }
+  },
+};
+
+export default gameController;
